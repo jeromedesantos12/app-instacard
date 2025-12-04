4b2968ac26650fb1f0972c96bd7b9c7f950ed907
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-12-04 15:51:04 +0700

    generate ulang prisma

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://localhost:3000/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"}/api/v1/:path*`,
       },
     ];
   },
